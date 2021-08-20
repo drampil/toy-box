a3ef82b77506e84badb768679594224b1f409bd6
David <77647013+drampil@users.noreply.github.com>
2021-08-20 09:06:11 -0400

    Update identity.sh
    added column formatting

--- identity.sh
+++ identity.sh
@@ -13,7 +13,7 @@ host=$(hostname)
 cpu=$(cat /proc/cpuinfo | grep "model name" | sed 's/[^,:]*://g')
 memavail=$(cat /proc/meminfo | grep "MemAvailable" | sed 's/[^*,:]*://g' | sed 's/^ *//g')
 memfree=$(cat /proc/meminfo | grep "MemFree" | sed 's/[^*,:]*://g' | sed 's/^ *//g')
-devices=$(lsblk | awk '(NR>1)' > devices.txt; cat devices.txt | sed 's/└─/\t └─/g')
+devices=$(lsblk | awk '(NR>1)' > devices.txt; cat devices.txt | sed 's/└─/\t└─/g')
 packages=$(yum list installed | wc -l)
 skull=$(cat skull.txt)
 user=$(whoami)
@@ -26,9 +26,9 @@ check=$(cat /etc/group | grep wheel | cut -d ":" -f 4)
 get_priviege(){
 if [[ $user = $check ]];
 then
-        echo "You have root power"
+        echo "You have root powers."
 else
-        echo "You don't have root power"
+        echo "You don't have any power here!"
 fi
 }
 
@@ -37,39 +37,39 @@ get_logo(){
 }
 
 get_user(){
-	echo "User: " $user"@"$system
+	echo "User:" $user"@"$system >> trash.txt
 }
 
 get_hostname(){
-	echo "Host: " $host
+	echo "Host:" $host  >> trash.txt
 }
 
 get_shell(){
-	echo "Shell: " $SHELL
+	echo "Shell:" $SHELL  >> trash.txt
 }
 
 get_kernel(){
-	echo "Kernel: " $kernel
+	echo "Kernel:" $kernel  >> trash.txt
 }
 
 get_uptime(){
-	echo "Uptime: " $uptime
+	echo "Uptime:" $uptime  >> trash.txt
 }
 
 get_cpu(){
-	echo "CPU: " $cpu
+	echo "CPU:" $cpu  >> trash.txt
 }
 
 get_memory(){
-	echo "Memory: "$memfree"/"$memavail
+	echo "Memory:" $memfree"/"$memavail  >> trash.txt
 }
 
 get_devices(){
-	echo -e "Devices: ""$devices"
+	echo -e "Devices:""$devices"
 }
 
 get_packages(){
-	echo "Packages Installed: "$packages
+	echo "Packages:" $packages  >> trash.txt
 }
 
 # Run all the functions in this order
@@ -79,13 +79,20 @@ get_hostname
 get_shell
 get_kernel
 get_uptime
-get_cpu
+get_cpu 
 get_memory
 get_packages
+
+
+# Display it
+cat trash.txt | column -s":" -t
+echo
 get_devices
+get_priviege
 echo
 echo "Welcome back," $user
-get_priviege
+
 
 # Cleanup
 rm devices.txt
+rm trash.txt
